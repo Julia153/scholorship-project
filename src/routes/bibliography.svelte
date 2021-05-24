844cf2cc13596d50bcbed1d21779c846cac83fb1
Julia Wakeman <wakeman.j@rangiruru.school.nz>
2021-05-24 20:18:04 +1200

    Bibliography added

---
+++ src/routes/bibliography.svelte
@@ -0,0 +1,38 @@
+<style>
+  /*making the title be centered */
+  .title {
+    text-align: center;
+  }
+
+  /* Setting the background color, and height of the page*/
+  #body {
+    background-color: #80cddb;
+    height: 1000px;
+    margin: 0px;
+  }
+
+  img {
+      width: 20%;
+      height: auto;
+  }
+</style>
+
+<div id="body">
+  <h1 class="title">Bibliography</h1>
+
+  <div id="home">
+    <a href="/">
+      <img id="homeIcon" src="home.png" alt="home icon" />
+    </a>
+  </div>
+  <div>
+    Water, milk, juice and rabbit icons made by
+    <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+    from
+    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+  </div>
+  <p>
+    All other icons from
+    <a href="https://fontawesome.com/">Font Awesome</a>
+  </p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,10 @@
-<script>
-
-</script>
-
 <style>
+/*making the title be centered */
   .title {
     text-align: center;
   }
 
+/* changing the buttons to look how I want them from the bulma default button class*/
   .button {
     width: 300px;
     height: 300px;
@@ -18,6 +16,7 @@
     background-color: #f5f6f4;
   }
 
+/* Making all of the buttons centered on the screen*/
   #container1-3 {
     height: 200px;
     position: relative;
@@ -46,12 +45,14 @@
     transform: translateY(-50%);
   }
 
+/* Setting the background color, and height of the page*/
   #body {
     background-color: #80cddb;
     height: 1000px;
     margin: 0px;
   }
 
+/* Changing the color of the buttons for settings and to log out*/
   #logOutLink,
   #settingsLink {
     color: #1d3461;
@@ -62,16 +63,21 @@
   }
 </style>
 
+<!--Div for whole page to set background color-->
 <div id="body">
+<!--Setting my title to be my SkyShip logo-->
   <h1 class="title">
     <img
       style="height: 20%; width: auto; "
       src="SkyShipTitle.png"
       alt="SkyShip title words sky ship in clouds with a rocket launching" />
   </h1>
-
+<!--Button to logout-->
   <a id="logOutLink" href="/login">Log Out</a>
+  <!--Button to go to settings-->
   <a id="settingsLink" href="/settings">Settings</a>
+
+  <!--Buttons for emotions-->
   <div id="container1-3">
     <div id="vertical-center1-3">
       <a id="emotions" href="/emotions">
@@ -81,26 +87,27 @@
           
         </button>
       </a>
-
+<!--Button for places-->
       <a id="places" href="/places">
         <button class="button">
           <i style="font-size: 100px;" class="fas fa-map-marker-alt" />
           Places
         </button>
       </a>
-
+<!--Button for phrases-->
       <a id="phrases" href="/phrases">
         <button class="button"><i style="font-size: 100px;" class="far fa-comment"></i>Phrases</button>
       </a>
     </div>
   </div>
 
+<!--button for food-->
   <div id="container3-5">
     <div id="vertical-center3-5">
       <a id="food" href="/food">
         <button class="button"><i style="font-size: 100px;" class="fas fa-apple-alt"></i>Food</button>
       </a>
-
+<!--button for animals-->
       <a id="animals" href="/animals">
         <button class="button"><i style="font-size: 100px;" class="fas fa-dog"></i>Animals</button>
       </a>
